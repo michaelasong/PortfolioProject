c2172ccbb57788f971469a7c2ab6a9bb16f2b258
Olaoye Oluwatoyin Abayomi <mindprepopps@gmail.com>
2022-09-08 20:13:51 +0100

    Create logic for menu control

---
+++ app.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+function OpenBar() {
+    const closebtn = document.querySelector('.small-screen');
+    closebtn.style.display = 'flex';
+  }
+  
+  function CloseBar() {
+    const closebtn = document.querySelector('.small-screen');
+    closebtn.style.display = 'none';
+  }
