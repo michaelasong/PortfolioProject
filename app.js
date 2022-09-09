7cf0ac8bcd4a2c8df7251e2e26bc8df45fd5f895
michaelasong <michaelasong237@gmail.com>
2022-09-09 14:15:40 +0100

    Fix linter errors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 function OpenBar() {
-    const closebtn = document.querySelector('.small-screen');
-    closebtn.style.display = 'flex';
-  }
-  
-  function CloseBar() {
-    const closebtn = document.querySelector('.small-screen');
-    closebtn.style.display = 'none';
-  }
+  const closebtn = document.querySelector('.small-screen');
+  closebtn.style.display = 'flex';
+}
+
+function CloseBar() {
+  const closebtn = document.querySelector('.small-screen');
+  closebtn.style.display = 'none';
+}
